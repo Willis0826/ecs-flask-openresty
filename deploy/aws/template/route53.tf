8bb8b3d04d82bf02c43dd9d4d4a211dac7b24980
WillisChou <dsa663838@yahoo.com.tw>
2020-01-09 00:24:57 +0800

    Add route53 willischou.com. zone

---
+++ deploy/aws/template/route53.tf
@@ -0,0 +1,16 @@
+resource "aws_route53_zone" "willischou" {
+  name = "willischou.com."
+  comment = "Managed by Terraform"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.willischou.zone_id}"
+  name    = "www.willischou.com."
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.ecs-openresty.dns_name}"
+    zone_id                = "${aws_elb.ecs-openresty.zone_id}"
+    evaluate_target_health = true
+  }
+}
