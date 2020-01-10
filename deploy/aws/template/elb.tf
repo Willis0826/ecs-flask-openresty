7ee5bdfdac1a037a17ed1a6314c91e4f3a05b489
WillisChou <>
2020-01-10 18:07:35 +0800

    ELB health check with https

--- deploy/aws/template/elb.tf
+++ deploy/aws/template/elb.tf
@@ -22,7 +22,7 @@ resource "aws_elb" "ecs-openresty" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
+    target              = "HTTPS:443/"
     interval            = 30
   }
 
